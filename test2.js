7b099c5f2d8d7fea1f101020dca9f0f606ff732d
Archit Jain <architsupper@gmail.com>
2026-04-02 20:30:04 +0530

    this is new file test2

---
+++ test2.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!");
+console.log("This is a simple js application");
+console.log("This is a simple js application");
+console.log("This is a simple js application");
+console.log("This is a simple js application");
